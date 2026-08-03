8e80df478ed7040ae2b8b3e6759f2f781d41bd47
ndanyikevin <ndanyikevin@gmail.com>
2026-08-03 05:31:46 +0300

    switched to npm from bun

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+import { Navbar } from "./components/Nav";
 import "./app.css";
+
+import "@fontsource/inter"
 
 export default function App() {
   return (
     <Router
       root={props => (
         <>
-          <Nav />
+          <Navbar />
           <Suspense>{props.children}</Suspense>
         </>
       )}
